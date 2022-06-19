62dac0edeb24a3c5ae56d2a211b4a2ec21dc1191
Lom911 <lomaev_82@mail.ru>
2022-06-19 10:50:36 +0300

    Commit 1

--- Treining/src/RimGo.java
+++ Treining/src/RimGo.java
@@ -51,7 +51,7 @@ public class RimGo {
                      summ = summ - 10;
                  }
              }
-             for (int i = 1; i < arab.length; i++)
+             for (int i = 0; i < arab.length; i++)
                  if (summ == (arab[i])) {
                      resul = resul + String.join ("", rim[i]);
                         }
